a913324458608f95cc28bec2b53f54215d8d3544
Felipe Narvaez <gfnarvaezh@unal.edu.co>
2020-05-24 05:36:41 -0500

    nuevo template html

--- js/script.js
+++ js/script.js
@@ -22,3 +22,25 @@ $(document).ready(function() {
       
       });
 });
+
+window.addEventListener('load', () => {
+  var xhr= new XMLHttpRequest();
+  xhr.open('GET', '/template/header.html', true);
+  xhr.onreadystatechange= function() {
+      if (this.readyState!==4) return;
+      if (this.status!==200) return; // or whatever error handling you want
+      document.getElementById('header').innerHTML= this.responseText;
+  };
+  xhr.send();
+});
+
+window.addEventListener('load', () => {
+  var xhr= new XMLHttpRequest();
+  xhr.open('GET', '/template/footer.html', true);
+  xhr.onreadystatechange= function() {
+      if (this.readyState!==4) return;
+      if (this.status!==200) return; // or whatever error handling you want
+      document.getElementById('footer').innerHTML= this.responseText;
+  };
+  xhr.send();
+});
